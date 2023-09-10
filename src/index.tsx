76c488c114498b171b1c9cf9cdc19f365b674800
kazuki1023 <k.keio12569@gmail.com>
2023-09-10 19:08:58 +0900

    add auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,15 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-qy444555oaxj3y6p.jp.auth0.com"
+    clientId="9RwnQ07fZUZUK7uZ2uYrTAnhWNS4KL8H"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>
 );
 
