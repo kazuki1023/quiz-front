b808897d8837212662a9699afe2d3e9e8d7d7d74
kazuki1023 <k.keio12569@gmail.com>
2023-09-13 13:48:03 +0900

    add データをjson形式で取得してきた

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import LoginButton from './Auth/login';
 
 function App() {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost/api/v1')
+      .then(response => setData(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
   return (
-    <div>
-      <LoginButton />
-    </div>
+    <>
+      <div>
+        <LoginButton />
+      </div>
+      <div>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+      </div>
+    </>
   );
 }
 
