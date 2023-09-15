b395c594f940d094126229f5b226df0923877c34
kazuki1023 <k.keio12569@gmail.com>
2023-09-15 15:10:29 +0900

    add choicesコンポーネント

---
+++ src/components/quiz/parts/Choices.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+const choices = () => {
+  return (
+    <li className="p-quiz-box__answer__item">
+      <button className="p-quiz-box__answer__button js-answer" data-answer="0">
+        約28万人<FontAwesomeIcon icon={faArrowRight} />
+      </button>
+    </li>
+  )
+}
+
+export default choices
