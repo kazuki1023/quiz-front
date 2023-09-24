7d6799b32561ed7f08fde0ca0f1cf84051fa1b05
kazuki1023 <k.keio12569@gmail.com>
2023-09-24 15:39:49 +0900

    signUpの見た目完成

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Quizpage';
 import SignIn from './pages/SignInpage';
+import SignUp from './pages/SignUppage';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path={`/signIn`} element={<SignIn />} />
+        </Routes>
+        <Routes>
+          <Route path={`/signUp`} element={<SignUp />} />
         </Routes>
 
       </BrowserRouter>
