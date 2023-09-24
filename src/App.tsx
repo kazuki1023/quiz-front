d380c5cfc4991fef9a6a7548fc08b499232cc31f
kazuki1023 <k.keio12569@gmail.com>
2023-09-24 14:50:33 +0900

    add ローティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,18 @@
-import { useState, useEffect } from 'react';
-import React from 'react';
-import axios from 'axios';
-import './App.css';
-import LoginButton from './Auth/login';
-import Header from './components/user/Header';
-import Quiz from './components/quiz/Quiz';
-import Footer from './components/user/Footer';
+import './App.css'
+import {
+  Route,
+  Routes,
+  BrowserRouter
+} from 'react-router-dom';
+import Home from './pages/Quizpage';
 
 function App() {
-  const [data, setData] = useState<Array<{
-    id: number;
-    content: string;
-    img: string;
-    choices: Array<{ answer: string; valid: number; }>;
-  }>>([]);
-  useEffect(() => {
-  axios.get('http://localhost/api/v1')
-    .then(response => {
-      const fetchedData = response.data.data; // ここでAPIからのデータを取得
-
-      // fetchedDataをQuizProps.quizDataの形に整形
-      const formattedData = fetchedData.map((quiz: any) => {
-        return {
-          id: quiz.id,
-          content: quiz.content,
-          img: quiz.img,
-          choices: quiz.choices.map((choice: any) => {
-            return {
-              answer: choice.answer,
-              valid: choice.valid
-            };
-          })
-        };
-      });
-
-      setData(formattedData);
-    })
-    .catch(error => console.error('Error fetching data:', error));
-}, []);
   return (
-    <>
-      <Header />
-      <LoginButton />
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
-      <div className="p-quiz-container l-container">
-          {data.map((quiz, index) => (
-            <Quiz key={index} quizData={quiz} />
-          ))}
-        </div>
-      <Footer />
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path={``} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
