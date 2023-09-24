5f124d8663d7782ed661ddd438eff82a13b56e58
kazuki1023 <k.keio12569@gmail.com>
2023-09-24 16:53:25 +0900

    add apiからデータを取得して、取得したデータをtableに入れた

--- src/components/admin/Table/Quiz.tsx
+++ src/components/admin/Table/Quiz.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import *  as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,57 +6,55 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-// const [data, setData] = useState<Array<{
-//   id: number;
-//   content: string;
-//   img: string;
-//   choices: Array<{ answer: string; valid: number; }>;
-// }>>([]);
 
-const QuizTable = () =>  {
+const QuizTable = () => {
+  const [datas, setData] = useState<Array<{
+    id: number;
+    content: string;
+  }>>([]);
+  useEffect(() => {
+    axios.get('http://localhost/api/v1')
+      .then(response => {
+        const fetchedData = response.data.data; // ここでAPIからのデータを取得
+        // fetchedDataをQuizProps.quizDataの形に整形
+        const formattedData = fetchedData.map((quiz: any) => {
+          return {
+            id: quiz.id,
+            content: quiz.content,
+          };
+        });
+
+        setData(formattedData);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>問題番号</TableCell>
+            <TableCell align="left">問題文</TableCell>
+            <TableCell align="right">編集</TableCell>
+            <TableCell align="right">削除</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {datas.map((data) => (
             <TableRow
-              key={row.name}
+              key={data.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
+              <TableCell component="th" scope="rows">
+                {data.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell component="th" scope="rows">
+                {data.content}
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
