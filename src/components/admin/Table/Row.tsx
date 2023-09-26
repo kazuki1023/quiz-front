8f74eab48c1b82c08f0091358faae5fe3ff1da26
kazuki1023 <k.keio12569@gmail.com>
2023-09-26 18:09:17 +0900

    add 駆除ボタン

--- src/components/admin/Table/Row.tsx
+++ src/components/admin/Table/Row.tsx
@@ -2,6 +2,8 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Tooltip from '@mui/material/Tooltip';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface RowProps {
   data: {
@@ -23,11 +25,15 @@ const Row: React.FC<RowProps> = ({ data }) => {
       </TableCell>
       <TableCell component="th" scope="rows">
         <Tooltip title="編集、更新ができます" arrow>
-          <Button sx={{ p:0 }}>編集</Button>
+          <Button sx={{ p: 0 }}>編集</Button>
         </Tooltip>
       </TableCell>
       <TableCell component="th" scope="rows">
-        {/* {data.content} */}
+        <Tooltip title="削除">
+          <IconButton sx={{ p:0 }}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
       </TableCell>
 
     </TableRow>
