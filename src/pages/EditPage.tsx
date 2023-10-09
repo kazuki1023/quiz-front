ce85c5744fefc993ae1274ba2b07bec6a182061a
kazuki1023 <k.keio12569@gmail.com>
2023-10-09 13:10:32 +0900

    feat: :art: 編集画面にloading

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/admin/parts/Header';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Loader from '../components/Loader';
 const EditPage = () => {
+  const [isLoading, setIsLoading] = useState(true); // ローディング中かどうかの状態
+  const { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    axios.get(`http://localhost/api/v1/quiz/${id}`)
+      .then(response => {
+        console.log(response.data.data)
+        setIsLoading(false); // ローディングが終わったのでfalseに
+      }
+      )
+      .catch(error => console.error('Error fetching data:', error));
+  })
   return (
     <>
       <Header />
-      <form action="" method="put" >
-      </form>
+      {isLoading ? <Loader /> :
+        <form action="" method="put" >
+        </form>
+      }
     </>
   );
 }
