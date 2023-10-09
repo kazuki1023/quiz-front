79ecf486a2c18ef416c3a67c4ef3aa35104a6cf6
kazuki1023 <k.keio12569@gmail.com>
2023-10-09 19:52:54 +0900

    feat: :art: putしたデータをapiが求める形式に変更

---
+++ src/Api/TransformToApiFormat.tsx
@@ -0,0 +1,35 @@
+interface FormData {
+  content: string;
+  choice1: string;
+  choice2: string;
+  choice3: string;
+  correctChoice: 'choice1' | 'choice2' | 'choice3';
+}
+
+export const TransformToApiFormat = (formData: FormData) => {
+  const quiz = {
+    content: formData.content,
+    // imgや他の項目も必要であればここに追加
+  };
+
+  // choices部分の変換
+  const choices = [
+    {
+      answer: formData.choice1,
+      valid: formData.correctChoice === "choice1"
+    },
+    {
+      answer: formData.choice2,
+      valid: formData.correctChoice === "choice2"
+    },
+    {
+      answer: formData.choice3,
+      valid: formData.correctChoice === "choice3"
+    }
+  ];
+
+  return {
+    quiz: quiz,
+    choices: choices
+  };
+}
