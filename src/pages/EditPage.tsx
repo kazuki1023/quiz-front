3c612e881ad71c17f4af1f0d2df3d66f94d97d1b
kazuki1023 <k.keio12569@gmail.com>
2023-10-09 15:21:28 +0900

    feat: :art: submitした値を取得

--- src/pages/EditPage.tsx
+++ src/pages/EditPage.tsx
@@ -6,14 +6,23 @@ import axios from 'axios';
 import { Control, Controller, FieldValues, Path, useForm } from 'react-hook-form';
 import Loader from '../components/Loader';
 const validationRules = {
-  comment: {
+  content: {
     maxLength: { value: 1000, message: '1000文字以内で入力してください' },
+    required: '問題文を入力してください',
   },
+  choice: {
+    required: '選択肢を入力してください',
+    maxLength: { value: 1000, message: '1000文字以内で入力してください' },
+  }
 };
 const EditPage = () => {
   const [isLoading, setIsLoading] = useState(true); // ローディング中かどうかの状態
   const { id } = useParams<{ id: string }>();
   const { control, handleSubmit, formState: { errors } } = useForm<FieldValues>();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
 
   useEffect(() => {
     axios.get(`http://localhost/api/v1/quiz/${id}`)
@@ -30,11 +39,11 @@ const EditPage = () => {
     <>
       <Header />
       {isLoading ? <Loader /> :
-        <Box component="form" sx={{ p: 2, width: 4 / 5, mx: 'auto', my: 2 }}>
+        <Box component="form" sx={{ p: 2, width: 4 / 5, mx: 'auto', my: 2 }} onSubmit={handleSubmit(onSubmit)}>
           <Controller
             name={"content"}
             control={control}
-            rules={validationRules.comment}
+            rules={validationRules.content}
             render={({ field, fieldState }) => (
               <FormControl error={!!fieldState.error?.message} sx={{ width: '100%', m: 2 }}>
                 <TextField
@@ -53,15 +62,15 @@ const EditPage = () => {
             )}
           />
           <Controller
-            name={"content"}
+            name={"choice1"}
             control={control}
-            rules={validationRules.comment}
+            rules={validationRules.content}
             render={({ field, fieldState }) => (
               <FormControl error={!!fieldState.error?.message} sx={{ width: '100%', m: 2 }}>
                 <TextField
                   {...field}
                   error={fieldState.error ? true : false}
-                  label="選択肢"
+                  label="選択肢1"
                   variant="outlined"
                   fullWidth
                   rows={1}
@@ -74,15 +83,15 @@ const EditPage = () => {
             )}
           />
           <Controller
-            name={"content"}
+            name={"chioce2"}
             control={control}
-            rules={validationRules.comment}
+            rules={validationRules.content}
             render={({ field, fieldState }) => (
               <FormControl error={!!fieldState.error?.message} sx={{ width: '100%', m: 2 }}>
                 <TextField
                   {...field}
                   error={fieldState.error ? true : false}
-                  label="選択肢"
+                  label="選択肢2"
                   variant="outlined"
                   fullWidth
                   rows={1}
@@ -95,15 +104,15 @@ const EditPage = () => {
             )}
           />
           <Controller
-            name={"content"}
+            name={"choice3"}
             control={control}
-            rules={validationRules.comment}
+            rules={validationRules.content}
             render={({ field, fieldState }) => (
               <FormControl error={!!fieldState.error?.message} sx={{ width: '100%', m: 2 }}>
                 <TextField
                   {...field}
                   error={fieldState.error ? true : false}
-                  label="選択肢"
+                  label="選択肢3"
                   variant="outlined"
                   fullWidth
                   rows={1}
@@ -117,7 +126,7 @@ const EditPage = () => {
           />
           <Box sx={{ display: 'flex', justifyContent: 'center', my: 2}}>
             <Button variant="contained" type="submit" sx={{ mx: 'auto', w: 1 }}>
-              登録・更新
+              更新
             </Button>
           </Box>
         </Box>
