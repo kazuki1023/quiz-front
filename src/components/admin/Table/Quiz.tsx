62b62a4ad40ccfdf7d4df3c06a4ca2544cf69bf3
kazuki1023 <k.keio12569@gmail.com>
2023-10-09 12:07:09 +0900

    feat: :zap: admin画面でloadingを追加

--- src/components/admin/Table/Quiz.tsx
+++ src/components/admin/Table/Quiz.tsx
@@ -6,10 +6,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useState, useEffect ,} from 'react';
+import { useState, useEffect, } from 'react';
 import axios from 'axios';
 import Row from './Row';
 import { useNavigate } from 'react-router-dom';
+import Loader from '../../Loader';
 
 
 
@@ -18,6 +19,7 @@ const QuizTable = () => {
     id: number;
     content: string;
   }>>([]);
+  const [isLoading, setIsLoading] = useState(true); // ローディング中かどうかの状態
 
   useEffect(() => {
     axios.get('http://localhost/api/v1/quiz')
@@ -32,6 +34,7 @@ const QuizTable = () => {
         });
 
         setData(formattedData);
+        setIsLoading(false); // ローディングが終わったのでfalseに
       })
       .catch(error => console.error('Error fetching data:', error));
   }, []);
@@ -54,23 +57,27 @@ const QuizTable = () => {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>問題番号</TableCell>
-            <TableCell align="left">問題文</TableCell>
-            <TableCell align="left">編集</TableCell>
-            <TableCell align="left">削除</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {datas.map((data) => (
-            <Row key={data.id} data={data} onDelete={handleDeleteQuiz} onEdit={handleEditQuiz}/>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {isLoading ? <Loader /> :
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>問題番号</TableCell>
+                <TableCell align="left">問題文</TableCell>
+                <TableCell align="left">編集</TableCell>
+                <TableCell align="left">削除</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {datas.map((data) => (
+                <Row key={data.id} data={data} onDelete={handleDeleteQuiz} onEdit={handleEditQuiz} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      }
+    </>
   );
 }
 export default QuizTable;
